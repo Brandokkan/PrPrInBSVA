c848bae3d53352704efd341aec3412fa9f2843fd
Brandokkan <brandokkan@gmail.com>
2026-08-26 01:02:44 +0200

    added descriptions to metrics.py

--- prprin/metrics.py
+++ prprin/metrics.py
@@ -2,13 +2,32 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 
-# UNWEIGHTED_METRICS_NAMES = ("degree_centrality", "betweenness_centrality", "clustering")
+
 STRINGID_COL_NAME = "stringId"
 SCORE_NAMES = ("score", "nscore", "fscore", "pscore", "ascore", "escore", "dscore", "tscore")
-ZERO_DIVISION_DISTANCE = 1000000
+
 
 def calculate_unweighted_metrics(graph, index_df, verbose=False):
+    """Calculate key un-weighted metrics of a graph nodes
 
+    This function calculates key un-weighted metrics of the nodes of the 
+    given graph and outputs a pandas DataFrame associating each node to 
+    the calculated metrics.
+
+    Parameters
+    -------
+    graph: networkx.Graph
+        The graph containing the nodes of which the metrics will be calculated
+
+    index_df: pandas.DataFrame
+        The DataFrame containing the association of stringId to symbol. The one
+        stored in the 'proteins' container.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing the un-weighted metrics associeted to each node
+    """
     # input check
     if not isinstance(graph, nx.Graph):
         raise TypeError("graph must be a networkx Graph")
@@ -30,8 +49,46 @@ def calculate_unweighted_metrics(graph, index_df, verbose=False):
     return final_df
     
 
-def calculate_weighted_metrics(graph, index_df, w_type="score", verbose=False):
+def calculate_weighted_metrics(graph, index_df, w_type="score", zero_distance=1000000, verbose=False):
+    """Calculate key weighted metrics of a graph nodes
 
+    This function calculates key weighted metrics of the nodes of the 
+    given graph and outputs a pandas DataFrame associating each node to 
+    the calculated metrics.
+
+    The attribute present in the graph used as the weight is indicated between
+    parenteses
+
+    Parameters
+    -------
+    graph: networkx.Graph
+        The graph containing the nodes of which the metrics will be calculated
+
+    index_df: pandas.DataFrame
+        The DataFrame containing the association of stringId to symbol. The one
+        stored in the 'proteins' container.
+
+    w_type: str
+        Must be one of "score", "nscore", "fscore", "pscore", "ascore", "escore",
+        "dscore", "tscore".
+
+        It tells the function which attribute should it use as the weight
+
+    zero_distance: numeric
+        When calculating the weighted betweenness centrality, the weights are
+        treated as distances (not similarities, like the scores of STRING). So, 
+        they must be converted.
+
+        Some of the conversion functions give an error when zero is inputed. In
+        these cases, zero_distance is the number associated to the zero value.
+
+        It is advised to input a big number (default one million).
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing the weighted metrics associeted to each node
+    """
     # input check
     if not isinstance(graph, nx.Graph):
         raise TypeError("graph must be a networkx Graph")
@@ -51,11 +108,11 @@ def calculate_weighted_metrics(graph, index_df, w_type="score", verbose=False):
         name=f"weighted betweenness centrality (1 - {w_type})"
     )
     w_betweenes_centrality_rec = pd.Series(
-            nx.betweenness_centrality(graph, weight=lambda u, v, d: 1 / d[w_type] if d[w_type] != 0 else ZERO_DIVISION_DISTANCE),
+            nx.betweenness_centrality(graph, weight=lambda u, v, d: 1 / d[w_type] if d[w_type] != 0 else zero_distance),
             name=f"weighted betweenness centrality (1 / {w_type})"
         )
     w_betweenes_centrality_log = pd.Series(
-            nx.betweenness_centrality(graph, weight=lambda u, v, d: -np.log(d[w_type]) if d[w_type] != 0 else ZERO_DIVISION_DISTANCE),
+            nx.betweenness_centrality(graph, weight=lambda u, v, d: -np.log(d[w_type]) if d[w_type] != 0 else zero_distance),
             name=f"weighted betweenness centrality (-log({w_type}))"
         )
     w_clustering_centrality = pd.Series(nx.clustering(graph, weight=w_type), name=f"weighted clustering coefficient ({w_type})")
@@ -71,15 +128,41 @@ def calculate_weighted_metrics(graph, index_df, w_type="score", verbose=False):
 
 
 class MetricsPPI:
-    def calculate_metrics(self, used_weight="score"):
+    def calculate_metrics(self, used_weight="score", zero_distance=1000000):
+        """Calculate key metrics of a graph nodes
 
+        This method calculates key metrics of the nodes of the 
+        graph stored in the 'graph' container and saves them in the 
+        'node_data' container.
+        
+        The attribute present in the graph used as the weight is indicated between
+        parenteses in the column names.
+        
+        Parameters
+        -------
+        used_weight: str
+            Must be one of "score", "nscore", "fscore", "pscore", "ascore", "escore",
+            "dscore", "tscore".
+
+            It tells the function which attribute should it use as the weight
+
+        zero_distance: numeric
+            When calculating the weighted betweenness centrality, the weights are
+            treated as distances (not similarities, like the scores of STRING). So, 
+            they must be converted.
+
+            Some of the conversion functions give an error when zero is inputed. In
+            these cases, zero_distance is the number associated to the zero value.
+
+            It is advised to input a big number (default one million).
+        """
         # check if a graph exists
         if not isinstance(self.graph, nx.Graph):
             raise ValueError("there is no graph saved in the 'graph' container")
 
         # calculate key graph parameters
         unweighted_metrics = calculate_unweighted_metrics(self.graph, self.proteins)
-        weighted_metrics = calculate_weighted_metrics(self.graph, self.proteins, used_weight)
+        weighted_metrics = calculate_weighted_metrics(self.graph, self.proteins, used_weight, zero_distance)
 
         # create final DataFrame
         self.node_data = pd.merge(unweighted_metrics, weighted_metrics, on=["stringId", "symbol"])
@@ -92,5 +175,5 @@ if __name__ == "__main__":
                           ("c","e", {"score":1}), ("c","f", {"score":0.95})
                           ))
     inx_df = pd.DataFrame({STRINGID_COL_NAME:["a","b","c","d","e","f"], "symbol":["azz","bee","caz","dam","ez","fuc"]})
-    print(weighted_metrics(graph, inx_df))
-    unweighted_metrics(graph, inx_df, verbose=True)
+    print(calculate_weighted_metrics(graph, inx_df))
+    calculate_unweighted_metrics(graph, inx_df, verbose=True)
